8467dbba2b08cc547baa7ca7f585657cd455840a
Alexandre Laborie <alaborie@gmail.com>
2011-12-03 14:10:08 -0800

    First commit.

---
+++ Sources/SQLDatabase.h
@@ -0,0 +1,12 @@
+//
+//  SQLDatabase.h
+//  SQLiteKit
+//
+//  Created by Laborie Alexandre on 12/3/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+@interface SQLDatabase : NSObject {
+}
+
+@end
---
+++ Sources/SQLDatabase.m
@@ -0,0 +1,13 @@
+//
+//  SQLDatabase.m
+//  SQLiteKit
+//
+//  Created by Laborie Alexandre on 12/3/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "SQLDatabase.h"
+
+@implementation SQLDatabase
+
+@end
