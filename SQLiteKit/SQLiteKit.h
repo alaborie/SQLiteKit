c62434af9551a506465c43850131715f7d8d228f
Alexandre Laborie <alaborie@gmail.com>
2012-01-31 14:33:54 -0800

    Added a category for NSOperationQueue that retuns a database queue. Added a header that contains all the public header of the framework.

---
+++ SQLiteKit/SQLiteKit.h
@@ -0,0 +1,11 @@
+//
+//  SQLGlobal.h
+//  SQLiteKit
+//
+//  Created by Alexandre Laborie on 1/24/12.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "SQLDatabase.h"
+#import "SQLQuery.h"
+#import "SQLRow.h"
